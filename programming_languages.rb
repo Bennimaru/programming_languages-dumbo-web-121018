599d6374ab9272e60d87b4fc81696a299238cbbf
Benson Yu <bennywhyu@gmail.com>
2018-12-06 06:45:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require "pry"
+
 def reformat_languages(languages)
   # your code here
+  
+  new_hash={}
+  
+  languages.each do|styles,programming_language|
+    new_hash=programming_language
+  end
+  
+  new_hash[:clojure]={:style =>[:functional]}
+  
+  binding.pry
+  new_hash
+  
 end
