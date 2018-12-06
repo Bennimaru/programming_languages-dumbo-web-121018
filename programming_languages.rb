285d3b0631b4d3d37977d0217eeab95fedaa012c
Benson Yu <bennywhyu@gmail.com>
2018-12-06 06:30:47 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
   # your code here
+  
+  new_hash=[]
+  
+  languages.each do|styles,programming_language|
+    programming_language.each do
+  
+  end
+  
+  
+  new_hash
+  
 end
